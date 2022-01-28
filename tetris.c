0a01a4c31b677cfff702f4bd694d6997561f1dee
efindus <48939809+efindus@users.noreply.github.com>
2022-01-28 19:28:28 +0100

    Add screenManager
    Signed-off-by: efindus <48939809+efindus@users.noreply.github.com>

--- tetris.c
+++ tetris.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <termios.h>
 #include <unistd.h>
+#include <pthread.h>
 
 // Constants
 #define HEIGHT 20
@@ -14,13 +15,18 @@
 #define VOID "\x1b[0m  "
 #define BORDER "\x1b[44m  "
 
-struct Point {
+typedef struct Point {
 	int x, y;
-};
+} Point;
+
+typedef struct TetrominoState {
+	Point position;
+	int id, rotation;
+} TetrominoState;
 
 // Tetrominos
 // 0 -> O shape; 1 -> I shape; 2 -> L shape; 3 -> J shape; 4 -> S shape; 5 -> Z shape; 6 -> T shape
-struct Point tetrominos[][4] = {
+Point tetrominos[][4] = {
 	{ { 0, 0 }, { 1, 0 }, { 1, 1 }, { 0, 1 } },
 	{ { 0, 0 }, { 1, 0 }, { -1, 0 }, { -2, 0 } },
 	{ { 0, 0 }, { -1, 0 }, { 1, 0 }, { 1, 1 } },
@@ -36,14 +42,21 @@ char* tetrominoColors[] = {
 	"\x1b[48;5;226m  ",
 	"\x1b[48;5;51m  ",
 	"\x1b[48;5;208m  ",
-	"\x1b[44m  ",
+	"\x1b[48;5;20m  ",
 	"\x1b[42m  ",
 	"\x1b[41m  ",
 	"\x1b[45m  ",
 };
 
+// Globals
 char board[WIDTH + 2][HEIGHT + 1][25];
 char frameBuffer[WIDTH + 2][HEIGHT + 1][25];
+
+TetrominoState currentTetromino;
+
+pthread_t renderThread;
+pthread_cond_t drawCV = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t drawMutex = PTHREAD_MUTEX_INITIALIZER;
 
 void setupBoard() {
 	for (int x = 1; x < WIDTH + 1; x++)
@@ -74,8 +87,8 @@ void drawFrame() {
  * rotation -> (0, 1, 2, 3) ordered as in here https://cdn.wikimg.net/en/strategywiki/images/7/7f/Tetris_rotation_super.png
  * but for I shaped tetromino there are only two rotations
  */
-struct Point* getRotatedTetromino(int tetrominoId, int rotation) {
-	struct Point* returnValue = malloc(sizeof(struct Point) * 4);
+Point* getRotatedTetromino(int tetrominoId, int rotation) {
+	Point* returnValue = malloc(sizeof(Point) * 4);
 	for (int i = 0; i < 4; i++) returnValue[i] = tetrominos[tetrominoId][i];
 
 	if (tetrominoId == 1) {
@@ -97,13 +110,13 @@ struct Point* getRotatedTetromino(int tetrominoId, int rotation) {
 
 // Draws selected tetromino using contents of board array as background (it only ensures nothing is set out of the array, so no collision checks are performed here).
 // If tetrominoId is -1 it will ignore it and simply draw contents of the board array
-void drawTetromino(struct Point position, int tetrominoId, int rotation) {
+void drawTetromino(Point position, int tetrominoId, int rotation) {
 	for (int x = 0; x < WIDTH + 2; x++)
 		for (int y = 0; y < HEIGHT + 1; y++)
 			strcpy(frameBuffer[x][y], board[x][y]);
 
 	if (tetrominoId != -1) {
-		struct Point* tetromino = getRotatedTetromino(tetrominoId, rotation);
+		Point* tetromino = getRotatedTetromino(tetrominoId, rotation);
 		for(int i = 0; i < 4; i++) {
 			int x = position.x + tetromino[i].x + 1, y = position.y + tetromino[i].y + 1;
 			if (0 <= x && x < WIDTH + 2 && 0 <= y && y < HEIGHT + 1) strcpy(frameBuffer[x][y], tetrominoColors[tetrominoId]);
@@ -114,21 +127,42 @@ void drawTetromino(struct Point position, int tetrominoId, int rotation) {
 	drawFrame();
 }
 
+void *screenManager() {
+	drawTetromino(currentTetromino.position, currentTetromino.id, currentTetromino.rotation);
+
+	while(1) {
+		pthread_mutex_lock(&drawMutex);
+		pthread_cond_wait(&drawCV, &drawMutex);
+
+		drawTetromino(currentTetromino.position, currentTetromino.id, currentTetromino.rotation);
+
+		pthread_mutex_unlock(&drawMutex);
+	}
+}
+
+void setupScreenManager() {
+	pthread_create(&renderThread, NULL, screenManager, NULL);
+}
+
 void setupTermiosAttributes() {
 	struct termios old = {0};
 	if (tcgetattr(0, &old) < 0)
-		perror("[ERROR] tcsetattr()");
+		perror("[ERROR] tcgetattr()");
 
 	old.c_lflag &= ~ICANON;
 	if (tcsetattr(0, TCSANOW, &old) < 0)
-		perror("[ERROR] tcsetattr ICANON");
+		perror("[ERROR] tcsetattr()");
 }
 
 void initialize() {
+	currentTetromino.position = (Point){ 0, 0 };
+	currentTetromino.id = -1;
+	currentTetromino.rotation = 0;
+
 	setupBoard();
 	setupTermiosAttributes();
 
-	drawFrame();
+	setupScreenManager();
 }
 
 int main() {
@@ -138,36 +172,40 @@ int main() {
 	time.tv_sec = 0;
 	time.tv_nsec = 1000 * 1000 * (1000 / TPS);
 
-	int rotation = 0, currentTetromino = 4;
-	struct Point basePosition = { WIDTH / 2, HEIGHT / 4 * 3 };
+	currentTetromino.position = (Point){ WIDTH / 2, HEIGHT / 4 * 3 };
+	currentTetromino.id = 3;
+	currentTetromino.rotation = 0;
 
 	while(1) {
 		char x;
 		read(0, &x, 1);
 
+		pthread_mutex_lock(&drawMutex);
 		switch (x) {
 			case 'w': {
-				rotation++;
+				currentTetromino.rotation++;
+				currentTetromino.rotation %= 4;
 				break;
 			}
 			
 			case 's': {
-				basePosition.y--;
+				currentTetromino.position.y--;
 				break;
 			}
 
 			case 'a': {
-				basePosition.x--;
+				currentTetromino.position.x--;
 				break;
 			}
 
 			case 'd': {
-				basePosition.x++;
+				currentTetromino.position.x++;
 				break;
 			}
 		}
 
-		drawTetromino(basePosition, currentTetromino, rotation);
+		pthread_cond_signal(&drawCV);
+		pthread_mutex_unlock(&drawMutex);
 
 		// if (nanosleep(&time, NULL) < 0)
 		// 	perror("[ERROR] nanosleep()");
